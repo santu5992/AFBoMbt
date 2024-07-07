ed6ec64f50d2127523a8f37f54c6dee2281fb94b
santu5992 <sayantanmandal500@gmail.com>
2024-07-07 14:36:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -901,7 +901,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             try:
                 for file in files:
                     file_ids = file.file_id
-                    file_name = file.file_caption
+                    file_name = file.file_name
                     result = await Media.collection.delete_one({
                         '_id': file_ids,
                     })
