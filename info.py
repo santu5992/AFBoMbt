f2f49a7baa8317e8b7243799907b79537f56f361
santu5992 <sayantanmandal500@gmail.com>
2024-07-10 00:06:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ SHORTENER_API2 = environ.get("SHORTENER_API2", "13cd8cac8c4c24b7530e6d47e9ec30ce
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'publicearn.com')
 SHORTENER_API3 = environ.get("SHORTENER_API3", "13cd8cac8c4c24b7530e6d47e9ec30ce0607d4be")
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'publicearn.com')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "28800"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "28800"))
 
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
 QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
